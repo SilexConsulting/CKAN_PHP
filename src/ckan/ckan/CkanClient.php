5936d41f2a6f2eab0f7c5354bf03139a1d4bcdb7
Noel Sharpe <noels@radnetwork.co.uk>
2013-03-14 13:53:36 +0000

    Remove redundant array. Update baseUrl configuration parameter to match service definition.

--- src/ckan/ckan/CkanClient.php
+++ src/ckan/ckan/CkanClient.php
@@ -13,7 +13,7 @@ class CkanClient extends Client {
      * Factory method to create a new CkanClient
      *
      * The following array keys and values are available options:
-     * - base_url: Base URL of web service
+     * - baseUrl: Base URL of web service
      * - scheme:   URI scheme: http or https
      * - username: API username
      * - password: API password
@@ -25,10 +25,10 @@ class CkanClient extends Client {
     public static function factory($config = array())
     {
         $default = array(
-            'base_url' => '{scheme}://{username}.test.com/',
+            'baseUrl' => '{scheme}://{username}.test.com/',
             'scheme'   => 'https'
         );
-        $required = array('base_url');
+        $required = array('baseUrl');
         $config = Collection::fromConfig($config, $default, $required);
 
         $client = new self($config->get('base_url'), $config);
@@ -39,18 +39,4 @@ class CkanClient extends Client {
         return $client;
     }
 
-	/**
-	 * as per http://docs.ckan.org/en/ckan-1.8/api-v2.html#model-api
-	*/
-	private $resources = array(
-		"Dataset Register" => "rest/dataset",
-		"Dataset Entity" => "rest/dataset/@dataset@",
-		"Group Register" => "rest/group",
-		"Group Entity" => "rest/group/@group@",
-		"Tag Register" => "rest/tag",
-		"Tag Entity" => "rest/tag/@tag@",
-		"Revision Register" => "rest/revision",
-		"Revision Entity" => "rest/revision/@revision@",
-		"License List" => "rest/licenses",
-	);
 }
